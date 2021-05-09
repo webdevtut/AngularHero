d891d94f45a32bbf76c43cb297e391311fbc3d0f
Tushar Kadam <tushar@futurewise.co.in>
2021-05-09 15:20:21 +0530

    Added Navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,14 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id) as Hero;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
